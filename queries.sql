87c2145741027e92a03cc7c6c7aab9dfb2d8ae89
Aamir <aamirasaram2107@gmail.com>
2023-08-12 17:28:00 +0200

    Update negative weights

--- queries.sql
+++ queries.sql
@@ -30,3 +30,16 @@ BEGIN;
 DELETE FROM animals
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth < '2022-01-01';
+SAVEPOINT deleted_record;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO deleted_record;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
